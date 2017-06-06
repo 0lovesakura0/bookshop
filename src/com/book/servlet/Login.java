e4460fe9168fb343444a959a8a09ca753f2b2228
Administrator <Administrator@192.168.2.142>
2017-06-06 22:15:37 +0800

    登陆

--- src/com/book/servlet/Login.java
+++ src/com/book/servlet/Login.java
@@ -1,11 +1,16 @@
 package com.book.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.book.dao.DB;
+import com.book.util.MD5;
 
 /**
  * Servlet implementation class Login
@@ -35,7 +40,21 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		try {
+			response.setHeader("Content-type","text/html;charset=UTF-8");
+			String username = request.getParameter("username");
+			MD5 dm5 =new MD5();
+			String password = dm5.getMD5ofStr(request.getParameter("password"));
+			
+			String sql = "select * from user where name = ? and password = ?";
+			DB db =new DB();
+			db.executeQuery(sql, username,password);
+			response.getWriter().append("登陆成功！");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response.getWriter().append("登陆失败！");
+		}
 	}
 
 }
