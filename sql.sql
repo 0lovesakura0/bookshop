fa87b10ff77777240ce3bcb435ebd3cd3af19e6e
cherry <cherry@DESKTOP-VESBIP2>
2017-06-09 00:10:21 +0800

    修改建库脚本

--- sql.sql
+++ sql.sql
@@ -23,15 +23,17 @@ DROP TABLE IF EXISTS `book`;
 CREATE TABLE `book` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL COMMENT '书籍名称',
-  `type` int(11) NOT NULL COMMENT '类型',
+  `type` varchar(10) NOT NULL COMMENT '类型',
   `price` decimal(10,2) NOT NULL COMMENT '单价',
   `number` int(11) DEFAULT NULL COMMENT '书籍编号',
   `author` varchar(20) DEFAULT NULL COMMENT '作者',
   `content` varchar(500) DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 /*Data for the table `book` */
+
+insert  into `book`(`id`,`name`,`type`,`price`,`number`,`author`,`content`) values (1,'java编程思想','计算机','74.90',1,'sum','这本书很不错'),(2,'论毛泽东思想','政治','38.00',2,'毛泽东','这本书   还凑合'),(3,'狭义相对论','物理','85.00',3,'爱因斯坦','这本书你基本看不懂'),(4,'广义相对论','物理','89.00',4,'爱因斯坦','这本书你根本看不懂'),(5,'水浒传','小说','120.00',5,'施耐庵 ','少年不看水浒，老年不读三国'),(6,'哈利波特','小说','238.00',6,'哈利波特','The book is English book');
 
 /*Table structure for table `shopcart` */
 
