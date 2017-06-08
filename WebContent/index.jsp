1d74e8bb2b642b9ad251c802c5344ff0cf6d933e
cherry <cherry@DESKTOP-VESBIP2>
2017-06-08 21:35:03 +0800

    添加设置

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -11,32 +11,96 @@
 	rel="stylesheet" type="text/css" />
 <script src="<%=request.getContextPath()%>/scripts/miniui/miniui.js"
 	type="text/javascript"></script>
-<script type="text/javascript">
-	
-</script>
+
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>书城首页</title>
 </head>
 <body>
-
+	<div style="padding-bottom: 5px;">
+		<span>书籍名称：</span> <input type="text" id="key" /> <input
+			type="button" value="查找" onclick="search()" />
+	</div>
+	<div style="width: 100%;">
+		<div class="mini-toolbar" style="border-bottom: 0; padding: 0px;">
+			<table style="width: 100%;">
+				<tr>
+					<td style="width: 100%;"><a class="mini-button"
+						iconCls="icon-add" onclick="addRow()" plain="true" tooltip="增加...">增加</a>
+						<a class="mini-button" iconCls="icon-remove" onclick="removeRow()"
+						plain="true">删除</a> <span class="separator"></span> <a
+						class="mini-button" iconCls="icon-save" onclick="saveData()"
+						plain="true">保存</a></td>
+					<td style="white-space: nowrap;"><input id="key"
+						class="mini-textbox" emptyText="请输入书籍名称" style="width: 150px;"
+						onenter="onKeyEnter" /> <a class="mini-button" onclick="search()">查询</a>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
 	<div id="datagrid1" class="mini-datagrid"
-		style="width: 100%; height: 550px;"
-		url="/BookList" idField="id"
-		allowResize="true" sizeList="[20,30,50,100]" pageSize="20">
+		style="width:100%; height: 580px;" url="BookList" idField="id"
+		allowResize="true" pageSize="20" allowCellEdit="true"
+		allowCellSelect="true" multiSelect="true" editNextOnEnterKey="true"
+		editNextRowCell="true">
 		<div property="columns">
 			<div type="indexcolumn"></div>
-			<div field="loginname" width="120" headerAlign="center"
-				allowSort="true">员工帐号</div>
-			<div field="name" width="120" headerAlign="center" allowSort="true">姓名</div>
-			<div field="gender" width="100" renderer="onGenderRenderer"
-				align="center" headerAlign="center">性别</div>
-			<div field="salary" numberFormat="¥#,0.00" align="right" width="100"
-				allowSort="true">薪资</div>
-			<div field="age" width="100" allowSort="true" decimalPlaces="2"
-				dataType="float">年龄</div>
-			<div field="createtime" width="100" headerAlign="center"
-				dateFormat="yyyy-MM-dd" allowSort="true">创建日期</div>
+			<div type="checkcolumn"></div>
+			
+			<div field="name" width="120" headerAlign="center" allowSort="true">书籍名称</div>
+			<div field="type" width="100" renderer="onGenderRenderer"
+				align="center" headerAlign="center">类别</div>
+			<div field="price" numberFormat="¥#,0.00" align="right" width="100"
+				allowSort="true">价格</div>
+			<div field="author" width="100" allowSort="true" decimalPlaces="2"
+				dataType="float">作者</div>
+			<div field="number" width="100" allowSort="true" decimalPlaces="2"
+				dataType="float">书籍编号</div>
+			<div field="content" width="100" allowSort="true" decimalPlaces="2"
+				dataType="float">详情</div>
+			<div field="content" width="100" allowSort="true" decimalPlaces="2"
+				dataType="float">操作</div>
 		</div>
 	</div>
 </body>
+
+<script type="text/javascript">
+	$(function() {
+		mini.parse();
+		var grid = mini.get("datagrid1");
+		console.log("datagrid1");
+		grid.load();
+		function search() {
+			var key = document.getElementById("key").value;
+
+			grid.load({
+				key : key
+
+			});
+		}
+		$("#key").bind("keydown", function(e) {
+			if (e.keyCode == 13) {
+				search();
+				console.log(e)
+			}
+		});
+		///////////////////////////////////////////////////////
+		var Genders = [ {
+			id : 1,
+			text : '男'
+		}, {
+			id : 2,
+			text : '女'
+		} ];
+		function onGenderRenderer(e) {
+			console.log(e)
+			for (var i = 0, l = Genders.length; i < l; i++) {
+				var g = Genders[i];
+				if (g.id == e.value)
+					return g.text;
+			}
+			return "";
+		}
+	})
+</script>
 </html>
--- src/com/book/servlet/BookList.java
+++ src/com/book/servlet/BookList.java
@@ -1,11 +1,17 @@
 package com.book.servlet;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.book.dao.DB;
+import com.book.mobel.Book;
 
 /**
  * Servlet implementation class BookList
@@ -28,11 +34,37 @@ public class BookList extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.setHeader("Content-type", "text/html;charset=UTF-8");
-		// TODO Auto-generated method stub
-		response.getWriter()
-				.append("{\"total\":24,\"maxAge\":37,\"avgAge\":26.2500,\"minAge\":0,\"data\":[{\"city\":\"\",\"age\":27,\"gender\":2,\"dept_id\":\"sc\",\"email\":\"wjm@qq.com\",\"position_name\":\"销售员\",\"salary\":\"3681\",\"createtime\":\"2012-01-31T10:53:40\",\"school\":\"河北科技大学\",\"name\":\"王佳美\",\"educational_name\":\"本科\",\"married\":1,\"educational\":\"2\",\"id\":\"12dfbe12-8a80-4bd6-9d88-f36b505408f8\",\"birthday\":\"1985-07-25T00:00:00\",\"country\":\"\",\"position\":\"sc3\",\"loginname\":\"wjm@qq.com\",\"remarks\":\"\",\"dept_name\":\"财务部\"},{\"city\":\"\",\"age\":25,\"gender\":1,\"dept_id\":\"js\",\"email\":\"wq@163.com\",\"position_name\":\"后台工程师\",\"salary\":\"3201\",\"createtime\":\"2012-01-31T10:53:39\",\"school\":\"天津商业大学\",\"name\":\"王强\",\"educational_name\":\"本科\",\"married\":0,\"educational\":\"2\",\"id\":\"92c2a9fb-5da1-4de5-a516-0c4c96d82ffd\",\"birthday\":\"1987-12-11T00:00:00\",\"country\":\"\",\"position\":\"js2\",\"loginname\":\"wq@163.com\",\"remarks\":\"\",\"dept_name\":\"技术部\"},{\"city\":\"\",\"age\":25,\"gender\":1,\"dept_id\":\"sc\",\"email\":\"sww@163.com\",\"position_name\":\"销售员\",\"salary\":\"3681\",\"createtime\":\"2012-01-31T10:53:35\",\"school\":\"天津科技大学\",\"name\":\"宋蔚伟\",\"educational_name\":\"本科\",\"married\":0,\"educational\":\"2\",\"id\":\"b3d3d2b2-460a-470e-a33c-aff1c8d8a652\",\"birthday\":\"1987-01-21T00:00:00\",\"country\":\"\",\"position\":\"sc3\",\"loginname\":\"sww@163.com\",\"remarks\":\"爱好篮球\",\"dept_name\":\"财务部\"},{\"city\":\"\",\"age\":27,\"gender\":1,\"dept_id\":\"cw\",\"email\":\"zm@163.com\",\"position_name\":\"会计\",\"salary\":\"3839\",\"createtime\":\"2012-01-31T10:50:12\",\"school\":\"贵州财经大学\",\"name\":\"张明\",\"educational_name\":\"本科\",\"married\":0,\"educational\":\"2\",\"id\":\"8197fb45-2b60-4309-820c-e70824fc9b33\",\"birthday\":\"1983-01-13T00:00:00\",\"country\":\"\",\"position\":\"cw2\",\"loginname\":\"zm@163.com\",\"remarks\":\"爱好足球\",\"dept_name\":\"财务部\"}]}")
-				.append(request.getContextPath());
+
+		try {
+			String name=request.getParameter("key");
+			String sql = "select * from book";
+			DB db = new DB();
+			ResultSet rs = null;
+			if ("".equals(name) || null==name) {
+				rs = db.executeQuery(sql, new Object[0]);
+			}else{
+				sql+="where name=? ";
+				rs = db.executeQuery(sql, name);
+			}
+			
+			String json = "{\"success\":true,\"data\":[";
+			while (rs.next()) {
+				json += "{\"id\":\"" + rs.getLong("id") + "\"," + "\"name\":\"" + rs.getString("name") + "\","
+						+ "\"price\":\"" + rs.getString("price") + "\",\"content\":\"" + rs.getString("content") + "\","
+						+ "\"number\":\"" + rs.getInt("number") + "\"," + "\"name\":\"" + rs.getString("name") + "\","
+						+ "\"author\":\"" + rs.getString("author") + "\"," + "\"type\":\"" + rs.getString("type")
+						+ "\"},";
+			}
+			json = json.substring(0, json.length() - 1) + "]}";
+			response.setHeader("Content-type", "text/html;charset=UTF-8");
+			// TODO Auto-generated method stub
+			response.getWriter().append(json);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response.setHeader("Content-type", "text/html;charset=UTF-8");
+			response.getWriter().append("{\"success\":false,\"msg\":\""+e.getMessage()+"\"");
+		}
 	}
 
 	/**
