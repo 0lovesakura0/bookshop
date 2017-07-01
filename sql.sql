3bf2f22e369f079445c43c2f7be9e304fd504e0d
iq <iq@iq-PC>
2017-07-01 18:34:50 +0800

    添加 详情

--- sql.sql
+++ sql.sql
@@ -1,9 +1,8 @@
-/*
+﻿/*
 SQLyog 企业版 - MySQL GUI v8.14 
-MySQL - 5.1.40-community : Database - bootshop
+MySQL - 5.1.40-community : Database - bookshop
 *********************************************************************
-*/
-
+*/
 /*!40101 SET NAMES utf8 */;
 
 /*!40101 SET SQL_MODE=''*/;
@@ -12,7 +11,7 @@ MySQL - 5.1.40-community : Database - bootshop
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`bootshop` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bookshop` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `bootshop`;
 
--- src/com/book/servlet/AddOrder.java
+++ src/com/book/servlet/AddOrder.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jdt.internal.compiler.ast.ThrowStatement;
-
 import com.book.dao.DB;
 
 @WebServlet("/addOrder")
